e51f6b96af135f13852848efc08826a04531d558
ahmed <ahm.798@proton.me>
2025-04-18 22:53:44 +0200

    ahmed: safe inser with mutex

--- safe_List_insert.c
+++ safe_List_insert.c
@@ -7,7 +7,7 @@ node * createNode(int data);
 void insert(node **head, int data);
 void printList(node *head);
 int listSize(node *head);
-void append(node **head, int data);
+
 
 struct node{
 	int data;
@@ -17,29 +17,37 @@ struct node{
 typedef struct{
 	int data;
 	node ** head;
+	pthread_mutex_t* lock;
 }ThreadArgs;
 
 
 void* threadfunction(void *arg){
 	ThreadArgs *args = (ThreadArgs *)arg;
-	append(args->head, args->data);
+	pthread_mutex_lock(args -> lock);
+	insert(args->head, args->data);
+	pthread_mutex_unlock(args -> lock);
 	return NULL;
 }
 
 int main(){
 	node* head = NULL;
 	
-	pthread_t threads[6];
-	ThreadArgs dargs[6];
+	pthread_t threads[3];
+	ThreadArgs * dargs = (ThreadArgs*) malloc(3 * sizeof(ThreadArgs));
 
-	for(int i = 0; i < 6; i++){
+	pthread_mutex_t lock;
+	pthread_mutex_init(&lock, NULL);
+
+	for(int i = 0; i < 3; i++){
 		dargs[i].data = i+1;
 		dargs[i].head = &head;
+		dargs[i].lock = &lock;
 		pthread_create(&threads[i], NULL, threadfunction, &dargs[i]);
 	}
-	for(int i = 0; i < 6; i++){
+	for(int i = 0; i < 3; i++){
 		pthread_join(threads[i], NULL);
 	}
+
 
 	printf("list length : %d\n", listSize(head));
 	printList(head);
@@ -78,18 +86,4 @@ int  listSize(node *head){
 		current = current -> next;
 	}
 	return len;
-}
-
-void append(node **head, int data){
-	node* newNode = createNode(data);
-	if (*head == NULL) {
-		*head = newNode;
-		return;
-	}
-	node* current = *head;
-	while (current->next != NULL) {
-		current = current->next;
-	}
-	current->next = newNode;
-	printf("Thread %ld: Inserted %d\n", pthread_self(), data);
 }
